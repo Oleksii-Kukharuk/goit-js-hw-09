2c325237c9ccd5e86d3c83a42170e25dd9a09a73
Oleksii-Kukharuk <104523043+Oleksii-Kukharuk@users.noreply.github.com>
2022-10-07 00:08:19 +0300

    tyazhko(((

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,41 +5,40 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const startBtnRef = document.querySelector('[data-start]');
 const timerRef = document.querySelector('.timer');
 
-// const DATA = new Date(2022, 9, 6, 23, 00);
+let DATA = null;
 
-flatpickr('#datetime-picker', {
+const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    if (selectedDates[0] < new Date()) {
+      // если выбраное время меньше текущего (прям вот текущего-текущего)
+      Notify.failure('Please choose a date in the future');
+      // вместо алерта выводим меседж
+      this.startBtn.disabled = true;
+      //делаем кнопку не активной (она может быть активной после прошлых действий)
+      return;
+      // обрываем функцию
+    }
+    timer.startBtn.classList.add('is_active');
+    //добавляем класс css (украшательство)
+    timer.startBtn.disabled = false;
+    //делаем кноку старт активной
+    timer.selectedDates = selectedDates[0];
   },
-});
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
+};
+flatpickr('#datetime-picker', options);
 
 const timer = {
   intervalId: null,
   refs: {},
   start(rootSelector, deadline) {
+    if (!deadline) {
+      return;
+    }
     const delta = deadline.getTime() - Date.now();
     if (delta <= 0) {
       Notify.failure(`Please choose a date in the future`, {
@@ -58,11 +57,40 @@ const timer = {
       closeButton: true,
     });
 
+    function convertMs(ms) {
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
+
+      const days = Math.floor(ms / day);
+      const hours = Math.floor((ms % day) / hour);
+      const minutes = Math.floor(((ms % day) % hour) / minute);
+      const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+      return { days, hours, minutes, seconds };
+    }
+
     this.getRefs(rootSelector);
 
     this.intervalId = setInterval(() => {
-      const delta = deadline.getTime() - Date.now();
-      console.log(convertMs(delta));
+      const diff = deadline.getTime() - Date.now();
+      if (diff <= 1000) {
+        clearInterval(this.intervalId);
+        Notify.success(`deadlin`, {
+          position: `center-center`,
+          backOverlay: true,
+          clickToClose: true,
+          closeButton: true,
+        });
+      }
+      const data = convertMs(diff);
+
+      console.log(data);
+      this.refs.days.textContent = this.zeroAdd(data.days);
+      this.refs.hours.textContent = this.zeroAdd(data.hours);
+      this.refs.minutes.textContent = this.zeroAdd(data.minutes);
+      this.refs.seconds.textContent = this.zeroAdd(data.seconds);
     }, 1000);
   },
   getRefs(rootSelector) {
@@ -71,6 +99,9 @@ const timer = {
     this.refs.minutes = rootSelector.querySelector('[data-minutes]');
     this.refs.seconds = rootSelector.querySelector('[data-seconds]');
   },
+  zeroAdd(value) {
+    return String(value).padStart(2, '0');
+  },
 };
 
-timer.start(timerRef, DATA);
+startBtnRef.addEventListener('click', timer.start(timerRef, DATA));
