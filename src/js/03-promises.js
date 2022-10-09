906bb9b75f41c621cc01301b0caf7ebb59ee7e94
Oleksii-Kukharuk <104523043+Oleksii-Kukharuk@users.noreply.github.com>
2022-10-10 01:30:11 +0300

    3 finish

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef = document.querySelector('form');
+// const stepRef = document.querySelector('.step');
+// const amountRef = document.querySelector('.amount');
+// const btnRef = document.querySelector('button');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  const promisse = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve = Notify.success(`Fulfilled promise ${position} in ${delay}ms`); // Fulfill
+      } else {
+        reject = Notify.failure(`Rejcted promise ${position} in ${delay}ms`); // Reject
+      }
+    }, delay);
+  });
+}
+
+function clickHandle(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'BUTTON') {
+    return;
+  }
+
+  let {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  let delayTrue = Number(delay.value);
+  let stepTrue = Number(step.value);
+  let amountTrue = Number(amount.value);
+
+  for (let i = 1; i <= amountTrue; i += 1) {
+    createPromise(i, delayTrue);
+    delayTrue += stepTrue;
   }
 }
+
+formRef.addEventListener('click', clickHandle);
